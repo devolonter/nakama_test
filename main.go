11bea8e4fe7424de3928f95613079ae5c5e919ee
devolonter <devolonter@gmail.com>
2023-06-21 14:43:08 +0200

    Handle in and out payloads

--- main.go
+++ main.go
@@ -3,8 +3,35 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
 	"github.com/heroiclabs/nakama-common/runtime"
 )
+
+const (
+	CodeOk              = 0
+	CodeInvalidArgument = 3
+)
+
+const (
+	DefaultContentType    = "core"
+	DefaultContentVersion = "1.0.0"
+)
+
+var (
+	ErrJsonMarshal   = runtime.NewError("failed to marshal json", CodeInvalidArgument)
+	ErrJsonUnmarshal = runtime.NewError("failed to unmarshal json", CodeInvalidArgument)
+)
+
+type GetContentInPayload struct {
+	Type    string  `json:"type"`
+	Version string  `json:"version"`
+	Hash    *string `json:"hash"`
+}
+
+type GetContentOutPayload struct {
+	GetContentInPayload
+	Content *string `json:"content"`
+}
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB,
 	nk runtime.NakamaModule, initializer runtime.Initializer) error {
@@ -14,5 +41,31 @@ func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB,
 
 func GetContentRpc(ctx context.Context, logger runtime.Logger, db *sql.DB,
 	nk runtime.NakamaModule, payload string) (string, error) {
-	return "{}", nil
+	in, err := bindPayload(payload)
+	if err != nil {
+		logger.Error("Failed to bind payload: %s", err.Error())
+		return "", ErrJsonUnmarshal
+	}
+
+	out := GetContentOutPayload{
+		GetContentInPayload: in,
+	}
+
+	result, err := json.Marshal(out)
+	if err != nil {
+		logger.Error("Failed to marshal json: %s", err.Error())
+		return "", ErrJsonMarshal
+	}
+
+	return string(result), nil
+}
+
+func bindPayload(payload string) (GetContentInPayload, error) {
+	in := GetContentInPayload{
+		Type:    DefaultContentType,
+		Version: DefaultContentVersion,
+	}
+
+	err := json.Unmarshal([]byte(payload), &in)
+	return in, err
 }
